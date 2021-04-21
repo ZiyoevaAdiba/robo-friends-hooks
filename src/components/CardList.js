2cbc2682622b1999d621aa0bd0f0e3f0ee2da6fb
Adiba <mac@Macs-MacBook-Pro.local>
2021-04-21 17:06:40 +0500

    fixing bugs

---
+++ src/components/CardList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({robots}) => {
+  return (
+  	<> 
+		{
+			robots.map((robot, i) => {
+				return (
+					<Card 
+						key={i} 
+						id={robot.id} 
+						name={robot.name} 
+						email={robot.email} 
+					/>
+				);
+			})		
+		}	
+    </>
+  );
+}
+
+export default CardList;
