d136d36989073e344b5744166477d765d4072220
Adiba <mac@Macs-MacBook-Pro.local>
2021-04-22 14:34:38 +0500

    cors issue

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,7 @@ class App extends Component {
 
 	componentDidMount() {
 		fetch('https://jsonplaceholder.typicode.com/users', {
-			mode: 'cors'
+			mode: 'no-cors'
 		})
 			.then(response => response.json())
 			.then(users => this.setState({robots: users}));
